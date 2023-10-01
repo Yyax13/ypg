492c24eccb7a70ca3e6d8f7481a5a3f0fd1b7a71
Yyax13 <138953949+Yyax13@users.noreply.github.com>
2023-10-01 19:14:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ module.exports = {
     
     readmeM = """# YPG
 
-> YProjects Gerenciator
+> YProjects Gerenciator (Linux only)
 
 ## How to use
 
@@ -98,25 +98,20 @@ clone the repo or download the project zip archive
 
 go to the directory where ypg has been install
 
-in windows and linux: `python ypg.py --[project-type] -n [name]`
-
-the final project will appear in the `/output` directory
+run this command on terminal: `sudo bash install.sh` (needs sudo)
 
 ## Web Mode
-to create the web project, run ```python ypg.py -w -n [name]``` or ```python ypg.py --web -n [name]```
+to create the web project, run ```ypg -w -n [name]``` or ```ypg --web -n [name]```
 
 to run the auto-included non-php server, run ```python server.py``` in terminal/cmd (on the project directory), and the server will start in <a href="http://localhost:5590">http://localhost:5590</a>
 
 ## Python terminal-side mode
-to create the project, run ```python ypg.py -pt -n [name]``` or ```python ypg.py --python-terminal -n [name]```
+to create the project, run ```ypg -pt -n [name]``` or ```ypg --python-terminal -n [name]```
 
 to run the python terminal-side project, run ```python main.py``` in terminal/cmd (on the project directory), and the server will start
 
 ## LICENSE
-You can use ypg for non-comercial projects and comercial use, its open for everyone, you can do what you wants
-- no tax
-- no limits
-Welcome to the revenge
+We Use the [[MIT LICENSE](LICENSE)](https://github.com/Yyax13/ypg/blob/main/LICENSE)
 
 ## Support
 I'm the support, contact-me on discord or telegram
@@ -194,7 +189,7 @@ def sair():
 
     readmeM = """# YPG
 
-> YProjects Gerenciator
+> YProjects Gerenciator (Linux only)
 
 ## How to use
 
@@ -202,25 +197,20 @@ clone the repo or download the project zip archive
 
 go to the directory where ypg has been install
 
-in windows and linux: `python ypg.py --[project-type] -n [name]`
-
-the final project will appear in the `/output` directory
+run this command on terminal: `sudo bash install.sh` (needs sudo)
 
 ## Web Mode
-to create the web project, run ```python ypg.py -w -n [name]``` or ```python ypg.py --web -n [name]```
+to create the web project, run ```ypg -w -n [name]``` or ```ypg --web -n [name]```
 
 to run the auto-included non-php server, run ```python server.py``` in terminal/cmd (on the project directory), and the server will start in <a href="http://localhost:5590">http://localhost:5590</a>
 
 ## Python terminal-side mode
-to create the project, run ```python ypg.py -pt -n [name]``` or ```python ypg.py --python-terminal -n [name]```
+to create the project, run ```ypg -pt -n [name]``` or ```ypg --python-terminal -n [name]```
 
 to run the python terminal-side project, run ```python main.py``` in terminal/cmd (on the project directory), and the server will start
 
 ## LICENSE
-You can use ypg for non-comercial projects and comercial use, its open for everyone, you can do what you wants
-- no tax
-- no limits
-Welcome to the revenge
+We Use the [[MIT LICENSE](LICENSE)](https://github.com/Yyax13/ypg/blob/main/LICENSE)
 
 ## Support
 I'm the support, contact-me on discord or telegram
