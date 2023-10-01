855fa6118d6eb095ea9c519e684a4e5b79f138ca
Yyax13 <138953949+Yyax13@users.noreply.github.com>
2023-10-01 19:17:23 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Dê permissão de escrita ao arquivo /root/.bashrc
 sudo chmod 666 /root/.bashrc
 
-# Função para adicionar o alias em um arquivo .bashrc
 add_alias() {
     local user="$1"
     local alias_line="alias ypg='python $script_dir/main.py'"
@@ -16,28 +14,25 @@ add_alias() {
     if [ -f "$bashrc_file" ]; then
         if ! grep -qF "$alias_line" "$bashrc_file"; then
             echo "$alias_line" >> "$bashrc_file"
-            echo "Alias 'ypg' adicionado ao $bashrc_file do usuário $user."
+            echo "Installed, run 'ypg' to see"
         else
-            echo "O alias 'ypg' já existe no $bashrc_file do usuário $user."
+            echo "Already installed at $user."
         fi
     else
-        echo "O arquivo $bashrc_file do usuário $user não foi encontrado. O alias não pôde ser adicionado."
+        echo "The $bashrc_file from $user can't be created. Run install.sh with sudo"
     fi
 }
 
-# Verifica se o sistema operacional é Linux
 if [[ "$(uname -s)" == "Linux" ]]; then
     # Obtém o diretório do script em execução
     script_dir="$(dirname "$(readlink -f "$0")")"
     
-    # Define o comando 'ypg' como um alias para 'python main.py' no diretório do script
-    echo "Alias 'ypg' será adicionado aos perfis do usuário atual e do usuário root."
+    echo "Installed, run 'ypg' to see"
     add_alias "$USER"
     add_alias "root"
   
-# Verifica se o sistema operacional é Windows (usando o Windows Subsystem for Linux)
 elif [[ "$(uname -s)" == "MINGW"* ]]; then
-    echo "Sistema operacional não suportado, por favor use Linux"
+    echo "Non-supported OS, pls use Unix distros"
 else
-    echo "Sistema operacional não suportado, por favor use Linux"
+    echo "Non-supported OS, pls use Unix distros
 fi
